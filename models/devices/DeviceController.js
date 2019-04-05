223b220a739bda0c3032e0e17544a8e934887282
haukda60920 <haukda60920@th-nuernberg.de>
2019-04-05 16:28:12 +0200

    reimplementet Light and DeviceController

--- models/devices/DeviceController.js
+++ models/devices/DeviceController.js
@@ -2,18 +2,7 @@ module.exports = function(devices) {
     this.devices = devices
 
     this.action = function(command) {
-
-        var deviceId = command.deviceId
-        var onoff = command.onoff
-
-        var d = devices.filter( device => device.id = deviceId);
-        switch (onoff) {
-            case "on":
-                d.on();
-                break;
-            case "off":
-                d.off()
-                break;
-        }
+        var d = devices.find( device => device.id == command.deviceId);
+        d.execute(command);
     }
 }
--- models/devices/light/Light.js
+++ models/devices/light/Light.js
@@ -2,6 +2,19 @@ module.exports = function(pin) {
     this.active = false;
     this.pin;
 
+    this.execute = function(command) {
+        switch (command.value) {
+            case "on":
+                this.on();
+                break;
+            case "off":
+                this.off();
+            default:
+                console.log("Undefined Command")
+                break;
+        }
+    }
+
     this.on = function() {
         this.active = true;
         console.log("Light on");
@@ -11,5 +24,9 @@ module.exports = function(pin) {
     this.off = function() {
         this.active = false;
         console.log("Light off");
+    }
+
+    this.isOn = function() {
+        return this.active;
     }
 }
