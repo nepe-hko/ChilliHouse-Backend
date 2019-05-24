3e88f43fe0d570f9575946e5c736074f3f7d7426
haukda60920 <haukda60920@th-nuernberg.de>
2019-05-24 16:26:43 +0200

    changed to mLab Database

--- start.js
+++ start.js
@@ -11,3 +11,4 @@ const app = require('./app');
 const server = app.listen(3000, () => console.log(`Webserver is running on port ${server.address().port}`));
 
 //mlab Datenbank: DATABASE=mongodb://danielhauk:passbox420@ds127376.mlab.com:27376/box420
+//Azure : DATABASE=mongodb://haukda60920:L2Vackm9j6HTSfpg2KqgE8TNCRrn5el6ILdoEMTtt9Qdx6LL2kmdrqDKLCqhjsiFQnFtNjDbdklzG4K5tn224w==@haukda60920.documents.azure.com:10255/?ssl=true
